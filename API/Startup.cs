4bc54910f391a9dbea5e0de0955df3972bfb598a
adurrant92 <adurrant92@gmail.com>
2022-02-27 19:14:16 +0000

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,14 +18,16 @@ namespace API
 {
     public class Startup
     {
+
         private readonly IConfiguration _config;
 
         public Startup(IConfiguration config)
         {
-           _config = config;
+            _config = config;
+
         }
 
-        
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -36,6 +38,7 @@ namespace API
 
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -55,6 +58,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            //  I am using policy instead of x 
 
             app.UseAuthorization();
 
